132da2e44cbcb76aa3a845dea23dfc2cc9a499af
Jack Pantalena <12242683+jpantalena@users.noreply.github.com>
2020-07-12 13:13:30 -0600

    Add warp to project and add base GET route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    // GET /
+    let root = warp::path::end().map(|| "Rust National Park");
+
+    warp::serve(root)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
